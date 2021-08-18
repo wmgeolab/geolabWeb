58d4982c74dc0d1d9c4ce4b10d1db8977f8bf8a0
DanRunfola <danr@wm.edu>
2021-08-18 12:16:54 -0400

    Automation Builds

---
+++ assets/python/generateProjectsPeople.py
@@ -0,0 +1,5 @@
+#Grab the CSVs
+ssID = "1g8L6cfelu4E4d8dAU8G8NgDER418alHFid69LP_axm0"
+
+peopleCSV = "https://docs.google.com/spreadsheets/d/" + ssID + "/gviz/tq?tqx=out:csv&sheet=" + "People"
+projectCSV = "https://docs.google.com/spreadsheets/d/" + ssID + "/gviz/tq?tqx=out:csv&sheet=" + "Projects"
